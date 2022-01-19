42b3298c8754e2f80e9322cd9d0511cae7deb946
gwni0214 <95908301+gwni0214@users.noreply.github.com>
2022-01-19 10:22:27 +0900

    2022.01.19

--- javascript/asset/js/script.js
+++ javascript/asset/js/script.js
@@ -26,14 +26,18 @@
 
 
 //결과 보기 토글
+    // document.querySelectorAll(".result").forEach(function(el){
+    //     el.addEventListener("click", function(){
+    //         document.querySelectorAll(".result > div").forEach(function(el){
+    //             el.classList.toggle("on")
+    //         });
+    //     });
+    // });
     document.querySelectorAll(".result").forEach(function(el){
         el.addEventListener("click", function(){
-            document.querySelectorAll(".result > div").forEach(function(el){
-                el.classList.toggle("on")
-            });
+            el.classList.toggle("active");
         });
     });
-    
     
         
    
