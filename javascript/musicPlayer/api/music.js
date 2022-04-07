725fd07316e72b206f83f57f0e5fb6973dae81ca
gwni0214 <sbxjs6999@naver.com>
2022-04-07 13:01:27 +0900

    0407

---
+++ javascript/musicPlayer/api/music.js
@@ -0,0 +1,122 @@
+let allMusic = [
+    {
+        name: "봄여름가을겨울 (Still Life)",
+        artist: "youtube_FreeMusic",
+        img: "image01",
+        audio: "song1"
+    },
+    {
+        name: "TOMBOY",
+        artist: "youtube_FreeMusic",
+        img: "image02",
+        audio: "song2"
+    },
+    {
+        name: "Feel My Rhythm",
+        artist: "youtube_FreeMusic",
+        img: "image03",
+        audio: "song3"
+    },
+    {
+        name: "GANADARA (Feat. 아이유)",
+        artist: "youtube_FreeMusic",
+        img: "image04",
+        audio: "song4"
+    },
+    {
+        name: "버퍼링 (Glitch Mode)",
+        artist: "youtube_FreeMusic",
+        img: "image05",
+        audio: "song5"
+    },
+    {
+        name: "SMILEY (Feat. BIBI)",
+        artist: "youtube_FreeMusic",
+        img: "image06",
+        audio: "song6"
+    },
+    {
+        name: "다시 사랑한다면 (김필 Ver.)",
+        artist: "youtube_FreeMusic",
+        img: "image07",
+        audio: "song7"
+    },
+    {
+        name: "strawberry moon",
+        artist: "youtube_FreeMusic",
+        img: "image08",
+        audio: "song8"
+    },
+    {
+        name: "Starlight",
+        artist: "youtube_FreeMusic",
+        img: "image09",
+        audio: "song9"
+    },
+    {
+        name: "music-10",
+        artist: "youtube_FreeMusic",
+        img: "image10",
+        audio: "song10"
+    },
+    {
+        name: "music-11",
+        artist: "youtube_FreeMusic",
+        img: "image11",
+        audio: "song11"
+    },
+    {
+        name: "music-12",
+        artist: "youtube_FreeMusic",
+        img: "image12",
+        audio: "song12"
+    },
+    {
+        name: "music-13",
+        artist: "youtube_FreeMusic",
+        img: "image13",
+        audio: "song13"
+    },
+    {
+        name: "music-14",
+        artist: "youtube_FreeMusic",
+        img: "image14",
+        audio: "song14"
+    },
+    {
+        name: "music-15",
+        artist: "youtube_FreeMusic",
+        img: "image15",
+        audio: "song15"
+    },
+    {
+        name: "music-16",
+        artist: "youtube_FreeMusic",
+        img: "image16",
+        audio: "song16"
+    },
+    {
+        name: "music-17",
+        artist: "youtube_FreeMusic",
+        img: "image17",
+        audio: "song17"
+    },
+    {
+        name: "music-18",
+        artist: "youtube_FreeMusic",
+        img: "image18",
+        audio: "song18"
+    },
+    {
+        name: "music-19",
+        artist: "youtube_FreeMusic",
+        img: "image19",
+        audio: "song19"
+    },
+    {
+        name: "music-20",
+        artist: "youtube_FreeMusic",
+        img: "image20",
+        audio: "song20"
+    },
+];
